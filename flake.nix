a404c3c5bd8b837b339218439f9ec4269b543a1b
thelissimus <thelissimus@tuta.io>
2025-11-17 10:02:26 +0500

    add: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,67 @@
 {
-  description = "Telegram bot assistant for @haskelluz community";
+  description = "Patchouli Haskell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { localSystem = { inherit system; }; };
-    in
-    {
-      devShells.default = pkgs.mkShell ({
-        buildInputs = with pkgs; [
-          zlib
-        ];
-      });
-    }
-  );
+  outputs =
+    { flake-parts
+    , git-hooks
+    , ...
+    }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { system, pkgs, ... }:
+        let
+          hpkgs = pkgs.haskell.packages.ghc912;
+
+          patchouli = pkgs.haskell.lib.overrideCabal (hpkgs.callCabal2nix "patchouli" ./. { }) (_: {
+            doCheck = true;
+            doHaddock = false;
+            enableLibraryProfiling = false;
+            enableExecutableProfiling = false;
+          });
+
+          githooks = git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              statix.enable = true;
+              deadnix.enable = true;
+              fourmolu.enable = true;
+              cabal-fmt.enable = true;
+              nixpkgs-fmt.enable = true;
+            };
+          };
+        in
+        {
+          packages.default = patchouli;
+
+          checks = {
+            inherit githooks;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              hpkgs.cabal-install
+              hpkgs.haskell-language-server
+              hpkgs.fourmolu
+              hpkgs.ghcid
+              hpkgs.ghc
+              pkgs.haskellPackages.cabal-fmt
+              pkgs.haskellPackages.implicit-hie
+            ];
+
+            shellHook = ''
+              ${githooks.shellHook}
+            '';
+          };
+        };
+    };
 }
